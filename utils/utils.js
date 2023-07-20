567159ba190ea92c6f44cb5861958d098481662b
VeronicaSteele <vmariesteele@gmail.com>
2023-07-19 21:04:46 -0500

    modal errors - not showing

--- utils/utils.js
+++ utils/utils.js
@@ -1,29 +1,28 @@
 const overlay = document.querySelector(".page");
 
 export function handleEscKey(evt) {
-	if (evt.key === "Escape") {
-		const activeModal = document.querySelector(".modal__opened");
-		activeModal;
-	}
+  if (evt.key === "Escape") {
+    const activeModal = document.querySelector(".modal__opened");
+  }
 }
 
 export function openModal(modal) {
-	modal.classList.add(".modal__opened");
-	document.addEventListener("keyup", handleEscKey);
-	overlay.addEventListener("mousedown", handlePopupClose);
+  modal.classList.add("modal__opened");
+  document.addEventListener("keyup", handleEscKey);
+  overlay.addEventListener("mousedown", handlePopupClose);
 }
 
 export function closeModal(modal) {
-	modal.classList.remove(".modal__opened");
-	document.removeEventListener("keyup", handleEscKey);
-	overlay.removeEventListener("mousedown", handlePopupClose);
+  modal.classList.remove("modal__opened");
+  document.removeEventListener("keyup", handleEscKey);
+  overlay.removeEventListener("mousedown", handlePopupClose);
 }
 
 export function handlePopupClose(evt) {
-	if (
-		evt.target.classList.contains("modal__overlay") ||
-		evt.target.classList.contains("modal__close")
-	) {
-		closeModal(evt.currentTarget);
-	}
+  if (
+    evt.target.classList.contains("modal__overlay") ||
+    evt.target.classList.contains("modal__close")
+  ) {
+    closeModal(evt.currentTarget);
+  }
 }
