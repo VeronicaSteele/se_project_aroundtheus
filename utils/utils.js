4386257afd0dfdc6ada7cb49b76f0c96dd6cf8ce
Joshua Zimmerman <Joshua.Zimmerman@lexialearning.com>
2023-07-19 20:57:07 -0500

    added in overlay

--- utils/utils.js
+++ utils/utils.js
@@ -1,27 +1,29 @@
+const overlay = document.querySelector(".page");
+
 export function handleEscKey(evt) {
-  if (evt.key === "Escape") {
-    const activeModal = document.querySelector(".modal__opened");
-    activeModal;
-  }
+	if (evt.key === "Escape") {
+		const activeModal = document.querySelector(".modal__opened");
+		activeModal;
+	}
 }
 
 export function openModal(modal) {
-  modal.classList.add(".modal__opened");
-  document.addEventListener("keyup", handleEscKey);
-  // overlay.addEventListener("mousedown", handlePopupClose);
+	modal.classList.add(".modal__opened");
+	document.addEventListener("keyup", handleEscKey);
+	overlay.addEventListener("mousedown", handlePopupClose);
 }
 
 export function closeModal(modal) {
-  modal.classList.remove(".modal__opened");
-  document.removeEventListener("keyup", handleEscKey);
-  // overlay.removeEventListener("mousedown", handlePopupClose);
+	modal.classList.remove(".modal__opened");
+	document.removeEventListener("keyup", handleEscKey);
+	overlay.removeEventListener("mousedown", handlePopupClose);
 }
 
 export function handlePopupClose(evt) {
-  if (
-    evt.target.classList.contains("modal__overlay") ||
-    evt.target.classList.contains("modal__close")
-  ) {
-    closeModal(evt.currentTarget);
-  }
+	if (
+		evt.target.classList.contains("modal__overlay") ||
+		evt.target.classList.contains("modal__close")
+	) {
+		closeModal(evt.currentTarget);
+	}
 }
