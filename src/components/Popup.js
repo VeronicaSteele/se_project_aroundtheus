5c7cc35e232a49a2198ea7d8baf45139f0ca6d67
VeronicaSteele <vmariesteele@gmail.com>
2023-08-04 10:04:06 -0500

    added:Popup and PopupWithForm pages

---
+++ src/components/Popup.js
@@ -0,0 +1,10 @@
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+  }
+  openpopup
+close popup
+handleEscClose
+setEventListeners 
+}
+
