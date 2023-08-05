faca6241b340e1eb41e27c2612fa7b5fabd6a712
VeronicaSteele <vmariesteele@gmail.com>
2023-08-04 20:36:44 -0500

    module not found error

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,29 +1,31 @@
+import PopupWithForm from "./PopupWithForm";
+
 export default class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
   }
- // this._overlay = document.querySelector(".page");
- // openpopup
-  function openModal(modal) {
+  // this._overlay = document.querySelector(".page");
+  // openpopup
+  openModal(modal) {
     modal.classList.add("modal_opened");
     document.addEventListener("keyup", handleEscKey);
     modal.addEventListener("mousedown", handlePopupClose);
   }
 
-//close popup
-function closeModal(modal) {
+  //close popup
+  closeModal(modal) {
     modal.classList.remove("modal_opened");
     document.removeEventListener("keyup", handleEscKey);
     modal.removeEventListener("mousedown", handlePopupClose);
   }
-//handleEscClose
-function handleEscKey(evt) {
+  //handleEscClose
+  _handleEscKey(evt) {
     if (evt.key === "Escape") {
       closeModal(document.querySelector(".modal_opened"));
     }
   }
 
-  function handlePopupClose(evt) {
+  handlePopupClose(evt) {
     if (
       evt.target.classList.contains("modal__overlay") ||
       evt.target.classList.contains("modal__close")
@@ -31,14 +33,5 @@ function handleEscKey(evt) {
       closeModal(evt.currentTarget);
     }
   }
-//setEventListeners 
+  //setEventListeners
 }
-
-
-
-
-
-
-
-
-
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,9 +1,9 @@
 import Popup from "./Popup.js";
 
-class PopupWithForm extends Popup {
+export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
-    super({ popupSelector });
-    this._popupForm = this._popupElement.querySelector(". modal__form");
+    super(popupSelector);
+    this._popupForm = this._popup.querySelector(".modal__form");
     this._handleFormSubmit = handleFormSubmit;
   }
   close() {
