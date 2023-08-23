cc70535a5ba31380ce27513e2858f363a4b240ab
VeronicaSteele <vmariesteele@gmail.com>
2023-08-22 22:37:31 -0500

    corrected BEM errors

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._image = this._popupElement.querySelector(".popup__image");
-    this._caption = this._popupElement.querySelector(".popup__caption");
+    this._image = this._popupElement.querySelector(".modal__image");
+    this._caption = this._popupElement.querySelector(".modal__caption");
   }
 
   openModal({ name, link }) {
