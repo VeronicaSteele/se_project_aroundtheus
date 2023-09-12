c2ac6f713a1e0285cbc9db9363b6134171e430af
VeronicaSteele <vmariesteele@gmail.com>
2023-09-11 21:37:42 -0500

    gitignore updated

---
+++ src/components/UserInfo.js
@@ -0,0 +1,17 @@
+export default class UserInfo {
+  constructor({ profileTitle, profileDescription }) {
+    this._profileTitle = profileTitle;
+    this._profileDescription = profileDescription;
+  }
+  getUserInfo() {
+    const userInfo = {
+      name: this._profileTitle.textContent,
+      description: this._profileDescription.textContent,
+    };
+    return userInfo;
+  }
+  setUserInfo({ name, description }) {
+    this._profileTitle.textContent = name;
+    this._profileDescription.textContent = description;
+  }
+}
